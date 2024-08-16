69c8a0c0d6de97f91bb374e83603a4fdd8713a3d
ruiiii <44080866+ruuuuuuuiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-08-16 13:06:52 +0800

    feat: add notification config

---
+++ services/notification/src/main/java/com/laureles/ecommerce/notification/NotificationRepository.java
@@ -0,0 +1,6 @@
+package com.laureles.ecommerce.notification;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+}
